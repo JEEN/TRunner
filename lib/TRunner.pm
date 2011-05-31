1a1396ff2601810f99374daeaadbaf87719bb0df
Jeen Lee <jeen@perl.kr>
2011-05-31 17:28:24 +0900

    git init

---
+++ lib/TRunner.pm
@@ -0,0 +1,30 @@
+package TRunner;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+get '/test-runner' => sub {
+  template 'test-runner', {}, { layout => undef };
+};
+
+get '/test-case/:name' => sub {
+    my $name = params->{name};
+    my $test_col = config->{selenium}->{TestCase}->{$name};
+    send_error("Not Found", 404) unless $test_col;
+    template 'test-case', { tcase => $test_col }, { layout => undef };
+};
+
+get '/test-suite' => sub {
+  template 'test-suite', { tsuite => config->{selenium}->{TestSuite} }, { layout => undef };
+};
+
+before_template sub {
+  my $tokens = shift;
+  $tokens->{uri_base} = '';
+};
+
+true;
