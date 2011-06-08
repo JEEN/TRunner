ca71a6d87480ca683a16d954c614f9714aec7a64
Jeen Lee <jeen@perl.kr>
2011-06-08 15:56:05 +0900

    * Simple Validation/Messages for PostResults in / * Set [ABSOLUTE] path on Selenium Configuration file for Plack mount * Removed { layout => undef } * USE Selenium configuration by $selenium

--- lib/TRunner.pm
+++ lib/TRunner.pm
@@ -2,11 +2,13 @@ package TRunner;
 use utf8;
 use Dancer ':syntax';
 use TRunner::Bridge::Smolder;
-
+use YAML::XS;
 our $VERSION = '0.1';
 
+my $selenium = YAML::XS::LoadFile(path(setting('confdir') || setting('appdir'), 'selenium.yml'));
+
 get '/' => sub {
-    template 'index';
+    template 'index', { config => config };
 };
 
 get '/test-runner' => sub {
@@ -15,21 +17,31 @@ get '/test-runner' => sub {
 
 get '/test-case/:name' => sub {
     my $name = params->{name};
-    my $test_col = config->{selenium}->{TestCase}->{$name};
+    my $test_col = $selenium->{TestCase}->{$name};
     send_error("Not Found", 404) unless $test_col;
-    template 'test-case', { tcase => $test_col }, { layout => undef };
+    template 'test-case', { tcase => $test_col };
 };
 
 get '/test-suite' => sub {
-  template 'test-suite', { tsuite => config->{selenium}->{TestSuite} };
+  template 'test-suite', { tsuite => $selenium->{TestSuite} };
 };
 
 post '/post-results' => sub {
   my %params = params;
+
+  my $is_passed = 0;
+  my $smolder = config->{smolder};
+     $smolder->{project_id} ||= $selenium->{project_id};
+  
+  $is_passed = 1 unless $smolder->{server};
+  $is_passed = 1 unless $smolder->{project_id};
+
+  return template 'post-results' if $is_passed;
+
   my $smolder = TRunner::Bridge::Smolder->new(
     "user_agent" => request->user_agent,
     "params"  => \%params,
-    "config"  => config->{smolder},
+    "config"  => $smolder,
   );
   $smolder->upload;
   template 'post-results';
