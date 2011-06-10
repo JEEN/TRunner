69a353e6accc5ad81e7a174f49ab2c6f711730d8
Jeen Lee <jeen@perl.kr>
2011-06-10 15:53:35 +0900

    minified code

--- lib/TRunner.pm
+++ lib/TRunner.pm
@@ -5,51 +5,47 @@ use TRunner::Bridge::Smolder;
 use YAML::XS;
 our $VERSION = '0.1';
 
-my $selenium = YAML::XS::LoadFile($ENV{SELENIUM_CONF_PATH} || path(setting('confdir') || setting('appdir'), 'selenium.yml'));
+my $selenium_conf = YAML::XS::LoadFile($ENV{SELENIUM_CONF_PATH} || path(setting('confdir') || setting('appdir'), 'selenium.yml'));
 
 get '/' => sub {
     template 'index', { config => config };
 };
 
 get '/test-runner' => sub {
-  template 'test-runner';
+    send_error("Bad Request", 400) unless params->{test};
+    template 'test-runner';
 };
 
 get '/test-case/:name' => sub {
     my $name = params->{name};
-    my $test_col = $selenium->{TestCase}->{$name};
+    my $test_col = $selenium_conf->{TestCase}->{$name};
     send_error("Not Found", 404) unless $test_col;
     template 'test-case', { tcase => $test_col };
 };
 
 get '/test-suite' => sub {
-  template 'test-suite', { tsuite => $selenium->{TestSuite} };
+  template 'test-suite', { tsuite => $selenium_conf->{TestSuite} };
 };
 
 post '/post-results' => sub {
   my %params = params;
 
   my $is_passed = 0;
-  my $smolder = config->{smolder};
-     $smolder->{project_id} ||= $selenium->{project_id};
+  my $smolder_conf = config->{smolder};
+     $smolder_conf->{project_id} ||= $selenium_conf->{project_id};
   
-  $is_passed = 1 unless $smolder->{server};
-  $is_passed = 1 unless $smolder->{project_id};
+  $is_passed = 1 unless $smolder_conf->{server};
+  $is_passed = 1 unless $smolder_conf->{project_id};
 
   return template 'post-results' if $is_passed;
 
   my $smolder = TRunner::Bridge::Smolder->new(
     "user_agent" => request->user_agent,
     "params"  => \%params,
-    "config"  => $smolder,
+    "config"  => $smolder_conf,
   );
   $smolder->upload;
   template 'post-results';
-};
-
-before_template sub {
-  my $tokens = shift;
-  $tokens->{uri_base} = request->uri_base;
 };
 
 true;
