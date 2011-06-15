aaf64e95b2550ce08dcebf3f31c8615df0ff79bd
Hyungsuk Hong <aanoaa@gmail.com>
2011-06-15 16:40:23 +0900

    Added 'share' dir for sharing static files
    copy all share/* files to public share dir when installed

--- lib/TRunner.pm
+++ lib/TRunner.pm
@@ -1,9 +1,19 @@
 package TRunner;
+# ABSTRACT: Selenium on Plack with Dancer
 use utf8;
 use Dancer ':syntax';
 use TRunner::Bridge::Smolder;
+use File::ShareDir qw/dist_dir/;
+use Config::General qw/ParseConfig/;
 use YAML::XS;
 our $VERSION = '0.1';
+
+## dynamic configuration here
+my $dist_dir = dist_dir('TRunner');
+if ($dist_dir) {
+    set views => "$dist_dir/views";
+    set public => "$dist_dir/public";
+}
 
 my $selenium_conf;
 
@@ -37,6 +47,11 @@ post '/post-results' => sub {
   my $is_passed = 0;
   my $smolder_conf = config->{smolder};
      $smolder_conf->{project_id} ||= $selenium_conf->{project_id};
+
+  if (-f "$ENV{HOME}/.trunner") {
+    my %config = ParseConfig("$ENV{HOME}/.trunner");
+    $smolder_conf = $config{smolder} if defined $config{smolder};
+  }
   
   $is_passed = 1 unless $smolder_conf->{server};
   $is_passed = 1 unless $smolder_conf->{project_id};
