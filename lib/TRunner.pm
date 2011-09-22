cd04a46b8c07ac0cafcbe12a87a9acb0189ebd54
Yongbin Yu <supermania@gmail.com>
2011-09-23 03:19:26 +0900

    Implement multiful selenium conf handle - #3

--- lib/TRunner.pm
+++ lib/TRunner.pm
@@ -4,45 +4,51 @@ use utf8;
 use Dancer ':syntax';
 use TRunner::Bridge::Smolder;
 use File::ShareDir qw/dist_dir/;
+use File::Basename qw/fileparse/;
 use Config::General qw/ParseConfig/;
 use YAML::XS;
 our $VERSION = '0.1';
 
-## dynamic configuration here
-my $dist_dir = dist_dir('TRunner');
-if ($dist_dir) {
-    set views => "$dist_dir/views";
-    set public => "$dist_dir/public";
-}
 
 my $selenium_conf;
 
 before sub {
-  $selenium_conf = YAML::XS::LoadFile($ENV{TRUNNER_CONF} || path(setting('confdir') || setting('appdir'), 'selenium.yml'));
+  my $dir = $ENV{TRUNNER_CONFDIR} ? $ENV{TRUNNER_CONFDIR} : 'seleinum/';
+  opendir my $confdir,$dir or die;
+  while(my $file = readdir($confdir)) {
+      next unless -f $file;
+      next unless $file =~ /\.ya?ml/i;
+      my ($filename) = fileparse($file, qr/\.[^.]*/);
+      $selenium_conf->{$filename} = YAML::XS::LoadFile($file) or die;
+  }
   if (-f "$ENV{HOME}/.trunner") {
     my %config = ParseConfig("$ENV{HOME}/.trunner");
     config->{smolder} = $config{smolder} if defined $config{smolder};
   }
+  closedir($confdir);
 };
 
 get '/' => sub {
-    template 'index', { config => config };
+    template 'index', { config => config, suites => [ keys %$selenium_conf ] };
 };
 
-get '/test-runner' => sub {
+get '/test-runner/:suite' => sub {
+    my $suite = params->{suite};
     send_error("Bad Request", 400) unless params->{test};
-    template 'test-runner';
+    template 'test-runner', { suite => $suite };
 };
 
-get '/test-case/:name' => sub {
+get '/test-case/:suite/:name' => sub {
+    my $suite = params->{suite};
     my $name = params->{name};
-    my $test_col = $selenium_conf->{TestCase}->{$name};
+    my $test_col = $selenium_conf->{$suite}->{TestCase}->{$name};
     send_error("Not Found", 404) unless $test_col;
-    template 'test-case', { tcase => $test_col };
+    template 'test-case', { tcase => $test_col, suite => $suite };
 };
 
-get '/test-suite' => sub {
-  template 'test-suite', { tcase=> $selenium_conf->{TestCase} };
+get '/test-suite/:suite' => sub {
+    my $suite = params->{suite};
+    template 'test-suite', { tcase=> $selenium_conf->{$suite}->{TestCase}, suite => $suite };
 };
 
 post '/post-results' => sub {
